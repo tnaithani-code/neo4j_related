747ac2f752cd864d4e378dd5f686b13e862fdf40
U1090964 <tarun.naithani@sanofi.com>
2025-11-25 08:10:47 +0000

    first test pass

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from neo4j-related!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/dummy_pack/sum_tn.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def add_two_numbers(a: int, b: int) -> int:
+    """
+    Returns the sum of two integers.
+    
+    :param a: First integer
+    :param b: Second integer
+    :return: Sum of a and b
+    """
+    return a + b
